8832d2d757ec471b1466f0dc2caa93dee3b0fe2d
thee5176 <theerapong5176@gmail.com>
2025-07-25 19:23:14 +0900

    add gaps between input

--- src/pages/LedgerCreateForm.tsx
+++ src/pages/LedgerCreateForm.tsx
@@ -16,65 +16,65 @@ export default function LedgerCreateForm() {
 
     return (
         <FormGroup>
-            <FormControl>
-                <InputLabel htmlFor="date" required>
-                    Date
-                </InputLabel>
-                <OutlinedInput
-                    id="date"
-                    name="date"
-                    type="date"
-                    placeholder="John"
-                    autoComplete={currentDate}
-                    required
-                    size="small"
-                />
+            <FormControl sx={{py:3}}>
+            <InputLabel htmlFor="date" required>
+            Date
+            </InputLabel>
+            <OutlinedInput
+            id="date"
+            name="date"
+            type="date"
+            placeholder="John"
+            autoComplete={currentDate}
+            required
+            size="small"
+            />
             </FormControl>
-            <FormControl>
-                <InputLabel htmlFor="description" required>
-                    Description
-                </InputLabel>
-                <OutlinedInput
-                    id="description"
-                    name="description"
-                    type="description"
-                    placeholder="Snow"
-                    autoComplete="description"
-                    required
-                    size="small"
-                />
+            <FormControl sx={{py:3}}>
+            <InputLabel htmlFor="description" required>
+            Description
+            </InputLabel>
+            <OutlinedInput
+            id="description"
+            name="description"
+            type="description"
+            placeholder="Snow"
+            autoComplete="description"
+            required
+            size="small"
+            />
             </FormControl>
-            <FormControl>
-                <InputLabel id="balance-type">Type</InputLabel>
-                <Select
-                    labelId="balance-type"
-                    id="demo-select-small"
-                    value={balanceType}
-                    label="Age"
-                    onChange={handleDropDown}
-                >
-                    <MenuItem value="">
-                    <em>None</em>
-                    </MenuItem>
-                    <MenuItem value={'Debit'}>Debit</MenuItem>
-                    <MenuItem value={'Credit'}>Credit</MenuItem>
-                </Select>
+            <FormControl sx={{py:3}}>
+            <InputLabel id="balance-type">Type</InputLabel>
+            <Select
+            labelId="balance-type"
+            id="demo-select-small"
+            value={balanceType}
+            label="Age"
+            onChange={handleDropDown}
+            >
+            <MenuItem value="">
+            <em>None</em>
+            </MenuItem>
+            <MenuItem value={'Debit'}>Debit</MenuItem>
+            <MenuItem value={'Credit'}>Credit</MenuItem>
+            </Select>
             </FormControl>
-            <FormControl>
-                <InputLabel htmlFor="amount" required>
-                    Amount
-                    </InputLabel>
-                    <OutlinedInput
-                    id="amount"
-                    name="amount"
-                    type="number"
-                    placeholder="0.00"
-                    autoComplete="amount"
-                    required
-                    size="small"
-                />
+            <FormControl sx={{py:3}}>
+            <InputLabel htmlFor="amount" required>
+            Amount
+            </InputLabel>
+            <OutlinedInput
+            id="amount"
+            name="amount"
+            type="number"
+            placeholder="0.00"
+            autoComplete="amount"
+            required
+            size="small"
+            />
             </FormControl>
-            
+
         </FormGroup>
     );
 }
