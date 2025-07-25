ceb8d691b256da57f9aa9e23bd76be122c8edd30
thee5176 <theerapong5176@gmail.com>
2025-07-25 19:26:39 +0900

    improve input size consistency

--- src/pages/LedgerCreateForm.tsx
+++ src/pages/LedgerCreateForm.tsx
@@ -17,62 +17,60 @@ export default function LedgerCreateForm() {
     return (
         <FormGroup>
             <FormControl sx={{py:3}}>
-            <InputLabel htmlFor="date" required>
-            Date
-            </InputLabel>
-            <OutlinedInput
-            id="date"
-            name="date"
-            type="date"
-            placeholder="John"
-            autoComplete={currentDate}
-            required
-            size="small"
-            />
+                <InputLabel htmlFor="date" required>
+                    Date
+                </InputLabel>
+                <OutlinedInput
+                    id="date"
+                    name="date"
+                    type="date"
+                    placeholder="John"
+                    autoComplete={currentDate}
+                    required
+                />
             </FormControl>
             <FormControl sx={{py:3}}>
-            <InputLabel htmlFor="description" required>
-            Description
-            </InputLabel>
-            <OutlinedInput
-            id="description"
-            name="description"
-            type="description"
-            placeholder="Snow"
-            autoComplete="description"
-            required
-            size="small"
-            />
+                <InputLabel htmlFor="description" required>
+                    Description
+                </InputLabel>
+                <OutlinedInput
+                    id="description"
+                    name="description"
+                    type="description"
+                    placeholder="Snow"
+                    autoComplete="description"
+                    required
+                />
             </FormControl>
             <FormControl sx={{py:3}}>
-            <InputLabel id="balance-type">Type</InputLabel>
-            <Select
-            labelId="balance-type"
-            id="demo-select-small"
-            value={balanceType}
-            label="Age"
-            onChange={handleDropDown}
-            >
-            <MenuItem value="">
-            <em>None</em>
-            </MenuItem>
-            <MenuItem value={'Debit'}>Debit</MenuItem>
-            <MenuItem value={'Credit'}>Credit</MenuItem>
-            </Select>
+                <InputLabel id="balance-type">Type</InputLabel>
+                <Select
+                    labelId="balance-type"
+                    id="demo-select-small"
+                    value={balanceType}
+                    label="Age"
+                    onChange={handleDropDown}
+                    required
+                >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={'Debit'}>Debit</MenuItem>
+                    <MenuItem value={'Credit'}>Credit</MenuItem>
+                </Select>
             </FormControl>
             <FormControl sx={{py:3}}>
-            <InputLabel htmlFor="amount" required>
-            Amount
-            </InputLabel>
-            <OutlinedInput
-            id="amount"
-            name="amount"
-            type="number"
-            placeholder="0.00"
-            autoComplete="amount"
-            required
-            size="small"
-            />
+                <InputLabel htmlFor="amount" required>
+                    Amount
+                </InputLabel>
+                <OutlinedInput
+                    id="amount"
+                    name="amount"
+                    type="number"
+                    placeholder="0.00"
+                    autoComplete="amount"
+                    required
+                />
             </FormControl>
 
         </FormGroup>
