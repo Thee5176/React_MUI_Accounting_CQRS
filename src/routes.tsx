9bc1d595567ef6c341ed4a42afdeb30dc2028587
Theerapong Likhitsab <Theerapong5176@gmail.com>
2025-08-14 17:40:33 +0900

    route error not found to homepage

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./Layout";
 import GeneralLedgerView from "./pages/GeneralLedgerView";
 import LedgerEntryForm from "./pages/LedgerEntryForm";
+import NotFoundPage from "./pages/NotFoundPage";
 
 export const routes = createBrowserRouter([
     { 
         path: '/',
         element: <Layout />,
+        errorElement: <NotFoundPage/>,
         children: [
             { path: '/', element: <GeneralLedgerView />},
             { path: '/form', element: <LedgerEntryForm />},
