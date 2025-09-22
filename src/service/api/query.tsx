9d6ac79af1b6ff0cc97ce896b177343172ae550a
Theerapong Likhitsab <Theerapong5176@gmail.com>
2025-09-22 21:50:17 +0900

    remove auth hook

--- src/service/api/query.tsx
+++ src/service/api/query.tsx
@@ -4,35 +4,7 @@ import { axiosQueryClient } from ".";
 
 //Config Query API Endpoint
 export function AxiosQueryClientProvider({children}: {children: React.ReactNode}) {
-    useEffect (() => {
-      axiosQueryClient.interceptors.request.use((config) =>{
-  if (config.headers !== undefined) {
-      // ヘッダにアクセストークンを埋める
-      // const accessToken = getAccessToken()
-      // if (accessToken) {
-      //   config.headers.Authorization = `Bearer ${accessToken}`
-      // }
-    }
-        return config;
-    });
-
-    axiosQueryClient.interceptors.response.use(
-      (response: AxiosResponse) => {
-        return response
-      },
-      (error: AxiosError) => {
-        switch (error.response?.status) {
-          case 401:
-            // 401の場合はログイン画面にリダイレクト
-            // window.location.href = '/login'
-            break
-          default:
-            break
-        }
-        return Promise.reject(error);
-      }
-    )
-
+  useEffect (() => {
     //Config Query API Endpoint
     const requestQueryInterceptor = axiosQueryClient.interceptors.request.use((config) => {
         if (config.headers !== undefined) {
@@ -48,26 +20,25 @@ export function AxiosQueryClientProvider({children}: {children: React.ReactNode}
     const responseQueryInterceptor = axiosQueryClient.interceptors.response.use(
       (response: AxiosResponse) => {
         return response;
-      },
-      (error: AxiosError) => {
-        switch (error.response?.status) {
-          case 401:
-            // 401の場合はログイン画面にリダイレクト
-            // window.location.href = '/login'
-            break
-          default:
-            break
-        }
-        return Promise.reject(error);
+    },
+    (error: AxiosError) => {
+      switch (error.response?.status) {
+        case 401:
+          // 401の場合はログイン画面にリダイレクト
+          // window.location.href = '/login'
+          break
+        default:
+          break
       }
-    )
+      return Promise.reject(error);
+    });
 
-    return () => {
-      axiosQueryClient.interceptors.request.eject(requestQueryInterceptor)
-      axiosQueryClient.interceptors.response.eject(responseQueryInterceptor)
-    }
+  return () => {
+    axiosQueryClient.interceptors.request.eject(requestQueryInterceptor)
+    axiosQueryClient.interceptors.response.eject(responseQueryInterceptor)
+  }
 
-  }, [])
+  }, []);
 
   return(<>{children}</>);
 }
